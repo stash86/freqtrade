296de1a14f6ea954718dcf88ba0fa9d29f6d6b5f
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2025-01-08 14:35:03 +0900

    rd

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,8 +1,14 @@
-sudo docker-compose pull;
-sudo docker-compose stop freqtrade;
-sudo docker-compose up -d freqtrade;
-sleep 30;
-for n in {2..12};
+# sudo docker-compose pull;
+# sudo docker-compose stop freqtrade;
+# sudo docker-compose up -d freqtrade;
+# sleep 30;
+for n in {2..10};
+do
+        sudo docker-compose stop freqtrade$n;
+        sudo docker-compose up -d freqtrade$n;
+        sleep 30;
+done
+for n in {16..21};
 do
         sudo docker-compose stop freqtrade$n;
         sudo docker-compose up -d freqtrade$n;
