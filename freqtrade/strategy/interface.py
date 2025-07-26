6e172365281c9482c9c0be2bc67c77fb214a4a35
Stefano <stefano.ariestasia@gmail.com>
2025-07-26 09:19:58 +0900

    debug config

--- freqtrade/strategy/interface.py
+++ freqtrade/strategy/interface.py
@@ -172,6 +172,7 @@ class IStrategy(ABC, HyperStrategyMixin):
                         "Informative timeframe must be equal or higher than strategy timeframe!"
                     )
                 if not informative_data.candle_type:
+                    print(config)
                     informative_data.candle_type = config["candle_type_def"]
                 self._ft_informative.append((informative_data, cls_method))
 
