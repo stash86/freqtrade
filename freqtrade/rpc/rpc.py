341eb0b23230d001408b150f81ecb764b8263b7f
Stefano <stefano.ariestasia@gmail.com>
2025-09-25 10:22:59 +0900

    fix

--- freqtrade/rpc/rpc.py
+++ freqtrade/rpc/rpc.py
@@ -1360,8 +1360,9 @@ class RPC:
                 if pair not in self._freqtrade.pairlists.whitelist:
                     try:
                         expand_pairlist([pair], list(self._freqtrade.exchange.get_markets().keys()))
-                        self._freqtrade.pairlists.whitelist.append(pair)
-                        self._freqtrade._refresh_active_whitelist(Trade.get_open_trades())
+                        self._freqtrade.active_pair_whitelist.append(pair)
+                        # self._freqtrade.pairlists.whitelist.append(pair)
+                        # self._freqtrade._refresh_active_whitelist(Trade.get_open_trades())
 
                     except ValueError:
                         errors[pair] = {"error_msg": f"Pair {pair} is not a valid wildcard."}
