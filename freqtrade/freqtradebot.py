4a84a2b6ecbd66284d7295be46929eeee3ab8c4b
Stefano <stefano.ariestasia@gmail.com>
2025-12-04 17:58:43 +0900

    cache min stake error

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -846,11 +846,16 @@ class FreqtradeBot(LoggingMixin):
 
             remaining = (trade.amount - amount) * current_exit_rate
             if min_exit_stake and remaining != 0 and remaining < min_exit_stake:
+                # msg = (
+                #     f"Trade #{trade.id} ({trade.pair}) - Remaining amount of {remaining} "
+                #     f"would be smaller than the minimum of {min_exit_stake}. Exit aborted."
+                # )
+                # logger.info(msg)
                 msg = (
-                    f"Trade #{trade.id} ({trade.pair}) - Remaining amount of {remaining} "
+                    f"Trade #{trade.id} ({trade.pair}) - Remaining amount "
                     f"would be smaller than the minimum of {min_exit_stake}. Exit aborted."
                 )
-                logger.info(msg)
+                self.log_once(msg, logger.info)
 
                 self.send_dp_message(msg)
 
